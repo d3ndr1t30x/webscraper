16ca6e4aa79f7ea9e067c2824af57d42cc99ae75
d3ndr1t30x <117501042+d3ndr1t30x@users.noreply.github.com>
2023-08-12 16:12:17 +1000

    Update scraper .py
    Updated to prompt the user for a filename

--- scraper .py
+++ scraper .py
@@ -2,8 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-# Replace this with the URL of the website you want to scrape
-website_url = "https://example.com/architectural-firms"
+# Prompt the user for the URL to scrape
+website_url = input("Enter the URL to scrape: ")
 
 # Send a GET request to the website and get the HTML content
 response = requests.get(website_url)
@@ -15,6 +15,15 @@ soup = BeautifulSoup(html_content, "html.parser")
 # Find all <a> tags (links) in the HTML
 links = soup.find_all("a")
 
+# Use a set to store unique links
+unique_links = set()
+
+# Iterate through links and add unique ones to the set
+for link in links:
+    href = link.get("href")
+    if href and href.startswith("http"):  # To filter out external links
+        unique_links.add(href)
+
 # Prompt the user for the output file name
 output_filename = input("Enter the output file name (without extension): ")
 
@@ -22,17 +31,15 @@ output_filename = input("Enter the output file name (without extension): ")
 text_output_filename = output_filename + ".txt"
 excel_output_filename = output_filename + ".xlsx"
 
-# Extract and save the href attribute of each link to the text file
+# Extract and save the unique links to the text file
 with open(text_output_filename, "w") as text_output_file:
-    for link in links:
-        href = link.get("href")
-        if href and href.startswith("http"):  # To filter out external links
-            text_output_file.write(href + "\n")
+    for href in unique_links:
+        text_output_file.write(href + "\n")
 
 print("Scraped links saved to", text_output_filename)
 
-# Create a DataFrame for the links and save to an Excel file
-data = {"Links": [link.get("href") for link in links if link.get("href") and link.get("href").startswith("http")]}
+# Create a DataFrame for the unique links and save to an Excel file
+data = {"Links": list(unique_links)}
 df = pd.DataFrame(data)
 df.to_excel(excel_output_filename, index=False)
 
